0ec0f1500d14ed55df5b1b6e16306e0ad9f0b1e1
dodo <saad9191h@gmail.com>
2022-08-19 14:50:34 -0400

    added password matching functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//for light/dark mode
+
 let themeButton = document.querySelector('.theme-toggle-but');
 let themeName = document.querySelector('.theme-name');
 let root = document.querySelector('html');
@@ -7,4 +9,24 @@ themeButton.addEventListener('click', () => {
     
     root.className = newTheme;
     themeName.textContent = newTheme;
+});
+
+
+//for password matching
+let pw = document.querySelector('#pw');
+let confirmPW = document.querySelector('#confirm-pw');
+
+confirmPW.addEventListener('input', () => {
+
+    if(confirmPW.value != pw.value) {
+        pw.classList.add('no-pw-match');
+        confirmPW.classList.add('no-pw-match');
+    }
+
+    else {
+        pw.classList.remove('no-pw-match');
+        confirmPW.classList.remove('no-pw-match');
+    }
+
 })
+
